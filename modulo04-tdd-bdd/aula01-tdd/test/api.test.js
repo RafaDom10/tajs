2700265e0f5fea6f7cb274818356f4bb57253d12
Rafael Domingues <r-domingues@outlook.com>
2025-02-18 18:25:00 -0300

    feat: add test when create a young-adult user

--- modulo04-tdd-bdd/aula01-tdd/test/api.test.js
+++ modulo04-tdd-bdd/aula01-tdd/test/api.test.js
@@ -1,4 +1,6 @@
-import { it, expect, describe } from '@jest/globals'
+import { it, expect, describe, beforeAll, afterAll, jest, beforeEach, afterEach } from '@jest/globals'
+import { server } from '../src/api.js'
+
 /**
  * - Deve cadastrar usuarios e definir uma categoria onde:
  *  - Jovens adultos:
@@ -12,8 +14,83 @@ import { it, expect, describe } from '@jest/globals'
  */
 
 describe('API Users E2E Suite', () => {
-    it.todo('should register a new user with young-adult category');
+
+    function waitForServerStatus(server) {
+        return new Promise((resolve, reject) => {
+            server.once('error', (err) => reject(err))
+            server.once('listening', () => resolve())
+        })
+    }
+
+    function createUser(data) {
+        return fetch(`${_testServerAddress}/users`, {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+    }
+
+    async function findUserById(id) {
+        const user = await fetch(`${_testServerAddress}/users/${id}`)
+        return user.json();
+    }
+
+    let _testServer
+    let _testServerAddress
+
+    beforeAll(async () => {
+        _testServer = server.listen()
+
+        await waitForServerStatus(_testServer)
+
+        const serverInfo = _testServer.address()
+        _testServerAddress = `http://localhost:${serverInfo.port}`
+    })
+
+    afterAll(done => {
+        server.closeAllConnections();
+        _testServer.close(done);
+    });
+
+    beforeEach(() => {
+        //* sempre mockar o tempo quando estiver trabalhando com datas
+        jest.useFakeTimers({
+            now: new Date('2025-02-18T00:00')
+        });
+    });
+
+    afterEach(() => {
+        //* afterEach é necessário para garantir que cada teste comece com um ambiente limpo, sem interferências de mocks de tempo anteriores.
+        jest.useRealTimers();
+    });
+
+    it('should register a new user with young-adult category', async () => {
+        const expectedCategory = 'young-adult';
+        const response = await createUser({
+            name: 'Xuxa da Silva',
+            birthDay: '2002-01-01'
+        })
+        expect(response.status).toBe(201);
+        const result = await response.json();
+        expect(result.id).not.toBeUndefined();
+
+        const user = await findUserById(result.id);
+        expect(user.category).toBe(expectedCategory)
+    });
+
     it.todo('should register a new user with adult category');
     it.todo('should register a new user with senior category');
-    it.todo('should throw an error when registering a under-age user');
+
+    it('should throw an error when registering a under-age user', async () => {
+        const response = await createUser({
+            name: 'Xuxa da Silva',
+            birthDay: '2020-01-01'
+        });
+
+        expect(response.status).toBe(400);
+
+        const result = await response.json();
+        expect(result).toEqual({
+            message: 'User must be 18yo or older'
+        })
+    });
 })
